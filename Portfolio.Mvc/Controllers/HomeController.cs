2b9969f54ea8aa7564e7a3ce12d90ec5bb992046
Ahmet Çiftçi <aciftci976@gmail.com>
2023-01-27 16:31:47 +0300

    Add project files.

---
+++ Portfolio.Mvc/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Portfolio.Mvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
